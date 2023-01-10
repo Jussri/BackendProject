9ad242db65629ef1ac9083cbc627b0e329401852
Jussi Rinta-Kiikka <jussi.rinta-kiikka@tuni.fi>
2023-01-10 15:04:09 +0200

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const expensesRouter = require("./src/routes/expenseRoute");
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5000",
+      "https://expense-express-api.onrender.com",
+    ],
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello");
